63931bf721101bea120a056255f9cb9ccf472539
elvin.mammedov <elvin.memmedov@a2z.az>
2023-12-28 13:47:39 +0400

    entities ready

---
+++ Core/OnionArch.Domain/Common/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnionArch.Domain.Common
+{
+    public class EntityBase
+    {
+        public int ID { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
